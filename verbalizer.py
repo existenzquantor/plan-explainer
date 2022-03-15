3404ed178bf41701354a9bf1a1bd71ef552f84ee
Felix Lindner <felix@existenzquantor.info>
2022-03-15 12:30:59 +0100

    .

--- verbalizer.py
+++ verbalizer.py
@@ -2,16 +2,13 @@ import sys
 from link_generator import getAllDLinks, getAllELinks
 
 if __name__ == "__main__":
-    print(sys.argv[1])
     my = __import__(sys.argv[1])
 
-    print("D-LINKS")
     dLinks = getAllDLinks(my.init, my.Plan, my.goal)
-    for d in dLinks:
-        print(d)
-
-
-    print("E-LINKS")
     eLinks = [x for x in getAllELinks(my.init, my.Plan, my.goal) if x not in dLinks]
-    for e in eLinks:
-        print(e)
+
+    dLinks = sorted(dLinks)
+    eLink = sorted(eLinks)
+
+    for i in range(-1, len(my.Plan)):
+        pass
