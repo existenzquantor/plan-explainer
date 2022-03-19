f3718a031c1ecd69fe92f2feb90cfd62c4acbd7e
Felix Lindner <felix@existenzquantor.info>
2022-03-19 10:25:58 +0100

    Simple verbalization strategy added.

--- verbalizer.py
+++ verbalizer.py
@@ -1,2 +1,30 @@
+
+def verbalize_enablers(eLinks):
+    loc = ""
+    for e in eLinks:
+        if e[0] == -1:
+            loc = loc + str(e[2]) + " holds initially and enables " + e[4] + ". "
+        else:
+            loc = loc + e[1] + " results in " + str(e[2]) + ", which enables " + e[4] + ". "
+    return loc
+
 def verbalize(dLinks, eLinks):
-    return ""
+    """
+    It is assumed that the links are already ordered in the order of verbalization.
+    """
+    glob = ""
+    loc = ""
+    step = -1
+    for d in dLinks:
+        if step != d[3]:
+            if step != -1:
+                loc = loc + verbalize_enablers([x for x in eLinks if x[0] == step])
+            step = d[3]
+            glob = glob + loc
+            loc = ""
+        if d[0] == -1:
+            loc = loc + str(d[2]) + " holds initially and requires " + d[4] + ". "
+        else:
+            loc = loc + d[1] + " results in " + str(d[2]) + ", which requires " + d[4] + ". "
+
+    return glob + loc + verbalize_enablers([x for x in eLinks if x[0] == step])
